efc0578b55982d52c32fa93d5fd3e8c620d6e87d
yasoo-exe <76653084+yasoo-exe@users.noreply.github.com>
2024-02-01 14:01:22 +0500

    complete ex# 2.6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,19 +4,36 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const addContact = (event) => {
+    event.preventDefault();
+    // const newContact = {
+    //   name: newName,
+    // };
+    setPersons(persons.concat([{ name: newName }]));
+    setNewName("");
+  };
+
+  const handleChange = (event) => {
+    setNewName(event.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addContact}>
         <div>
-          name: <input />
+          name: <input onChange={handleChange} value={newName} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map((person) => (
+          <li key={person.name}>{person.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
