5e304522e567e8cbda63996d439d948ab1900962
yasoo-exe <76653084+yasoo-exe@users.noreply.github.com>
2024-02-21 14:48:40 +0500

    complete ex 3.1 & ex 3.2

--- part3/index.js
+++ part3/index.js
@@ -1,2 +1,42 @@
-console.log("hello, it works!");
-console.log("hello, it works with nodemon as well!");
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const phonebook = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/api/phonebook", (request, response) => {
+  response.json(phonebook);
+});
+
+app.get("/info", (request, response) => {
+  const dataToRetun = `<p>Phonebook contains ${
+    phonebook.length
+  } people</p> <p>${new Date()}</p>`;
+  response.send(dataToRetun);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log("server is running");
+});
