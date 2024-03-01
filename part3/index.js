52e2f33697ca6de7f1de59f2f76d3ec241bc9406
yasoo-exe <76653084+yasoo-exe@users.noreply.github.com>
2024-03-01 10:51:45 +0500

    configure morgan middleware
    tiny configuration format for logging rest server requests to the console

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const app = express();
 app.use(express.json());
+
+app.use(morgan("tiny")); //morgan middleware to log messages to the console in tiny format
 
 let persons = [
   {
