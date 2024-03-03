06b1b2c3f66077dbcfb04af984ebca451434d6bc
yasoo-exe <76653084+yasoo-exe@users.noreply.github.com>
2024-03-03 13:33:18 +0500

    project: add cors

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 morgan.token("req-body", (req) => {
@@ -99,7 +102,7 @@ app.post("/api/persons", (request, response) => {
   }
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log("server is running");
+  console.log(`server is running at port ${PORT}`);
 });
