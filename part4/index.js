8d161ac9d78a8afa27fd75952e96e289cf8f6893
Yasir Mehmood <76653084+yasoo-exe@users.noreply.github.com>
2024-03-21 12:09:30 +0500

    complete ex 4.1

--- part4/index.js
+++ part4/index.js
@@ -1,42 +1,44 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
   url: String,
-  likes: Number
-})
+  likes: Number,
+});
 
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = mongoose.model("Blog", blogSchema);
 
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl)
+const mongoUrl = process.env.MONGODB_URI;
+mongoose
+  .connect(mongoUrl)
+  .then(() => {
+    console.log("connected to database successfully!");
+  })
+  .catch((error) => console.log("there was a fucking error!", error));
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+app.get("/api/blogs", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+app.post("/api/blogs", (request, response) => {
+  const blog = new Blog(request.body);
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
 
-const PORT = 3003
+const PORT = 3003;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`);
 });
